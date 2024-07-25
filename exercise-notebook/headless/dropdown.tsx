3b0d1b19545aedf407cb913bf70893a882959976
SeonghunYang <didtjdgns852@naver.com>
2024-07-25 03:52:24 +0000

    키보드 컨드롤 로직 추가

--- exercise-notebook/headless/dropdown.tsx
+++ exercise-notebook/headless/dropdown.tsx
@@ -14,13 +14,24 @@ const Dropdown = ({ items }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState<Item | null>(null);
 
+  const [selectedIndex, setSelectedIndex] = useState<number>(-1);
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    switch (
+      e.key
+      // ... 케이스 구문 ...
+      // ...  Enter, Space, ArrowDown and ArrowUp 키에 대한 핸들링 ...
+    ) {
+    }
+  };
+
   return (
-    <div className="dropdown">
+    <div className="dropdown" onKeyDown={handleKeyDown}>
       <Trigger
         label={selectedItem ? selectedItem.text : "Select an item..."}
         onClick={() => setIsOpen(!isOpen)}
       />
-      {isOpen && <DropdownMenu items={items} onItemClick={setSelectedItem} />}
+      {isOpen && <DropdownMenu selectedIndex={selectedIndex} items={items} onItemClick={setSelectedItem} />}
     </div>
   );
 };
@@ -41,13 +52,16 @@ const Trigger = ({
 
 const DropdownMenu = ({
   items,
+  selectedIndex,
   onItemClick,
 }: {
   items: Item[];
+  selectedIndex: number;
+
   onItemClick: (item: Item) => void;
 }) => {
   return (
-    <div className="dropdown-menu">
+    <div className="dropdown-menu" role="listbox">
       {items.map((item, index) => (
         <div
           key={index}
