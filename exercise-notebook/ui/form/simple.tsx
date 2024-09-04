6b7dcb5734dbb691f8af32a379202cbb35433f4b
SeonghunYang <didtjdgns852@naver.com>
2024-09-04 06:19:12 +0000

    feat: Add SimpleForm component to exercise notebook app

--- exercise-notebook/ui/form/simple.tsx
+++ exercise-notebook/ui/form/simple.tsx
@@ -1,3 +1,57 @@
+"use client";
+import React, { useState } from "react";
+
 export default function SimpleForm() {
-  return <div></div>;
+  const [loginForm, setLoginForm] = useState({
+    id: "",
+    password: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginForm({
+      ...loginForm,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
+  return (
+    <form>
+      <Input
+        name="id"
+        label="아이디"
+        onChange={handleChange}
+        value={loginForm.id}
+      />
+      <Input
+        name="password"
+        label="비밀번호"
+        onChange={handleChange}
+        value={loginForm.password}
+      />
+      <input type="submit" />
+    </form>
+  );
+}
+
+interface InputProps {
+  label: string;
+  name: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function Input({ label, name, value, onChange }: InputProps) {
+  return (
+    <div>
+      <div>
+        <label htmlFor={`input-${name}`}>{label}</label>
+      </div>
+      <input
+        onChange={onChange}
+        value={value}
+        name={name}
+        id={`input-${name}`}
+      />
+    </div>
+  );
 }
