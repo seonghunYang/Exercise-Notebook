95962afeb866db2696e2846a3b88bf173359758f
SeonghunYang <didtjdgns852@naver.com>
2024-09-06 12:10:05 +0000

    SearchInput 컴포넌트 분리

---
+++ exercise-notebook/form/components/search-input.tsx
@@ -0,0 +1,64 @@
+import { Input, InputProps } from "@/ui/form/form-hook-controlled-form";
+import React, { useState } from "react";
+
+export interface SearchInputProps<T> extends Omit<InputProps, "type"> {
+  searchListData: T[];
+  searchRender: (data: T) => React.ReactNode;
+  searchFilter: (data: T) => boolean;
+}
+
+export function SearchInput<T>({
+  searchListData,
+  searchRender,
+  searchFilter,
+  ...props
+}: SearchInputProps<T>) {
+  const [open, setOpen] = useState(false);
+
+  const handleInputFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    props.onFocus?.(event);
+    setOpen(true);
+  };
+
+  const handleUnderlayClick = () => {
+    setOpen(false);
+  };
+
+  const searchedList = searchListData.filter(searchFilter);
+
+  return (
+    <div
+      style={{
+        position: "relative",
+      }}
+    >
+      {open && (
+        <div
+          style={{
+            position: "fixed",
+            inset: "0px",
+          }}
+          aria-hidden={true}
+          onClick={handleUnderlayClick}
+        ></div>
+      )}
+      <Input type={"search"} {...props} onFocus={handleInputFocus} />
+      {open && (
+        <div
+          style={{
+            position: "absolute",
+            backgroundColor: "white",
+          }}
+        >
+          {
+            <div>
+              {searchedList.map((data) => {
+                return searchRender(data);
+              })}
+            </div>
+          }
+        </div>
+      )}
+    </div>
+  );
+}
