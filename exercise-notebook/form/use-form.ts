8092424c0364ef7e8ff1249e516d527fdf81b936
SeonghunYang <didtjdgns852@naver.com>
2024-09-06 12:18:21 +0000

    use-form 훅 분리

---
+++ exercise-notebook/form/use-form.ts
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+
+interface UseFormArgs<T extends Record<string, any>> {
+  initialState: T;
+  validateFn: (formState: T) => ErrorMessage<T>;
+}
+
+type ErrorMessage<T extends Record<string, any>> = {
+  [key in keyof T]: string;
+};
+
+export function useForm<T extends Record<string, any>>({
+  initialState,
+  validateFn,
+}: UseFormArgs<T>) {
+  const [formState, setFormState] = useState<T>(initialState);
+  const [wasSubmitted, setWasSubmitted] = useState<boolean>(false);
+
+  const initialErrorMessage = (Object.keys(initialState) as (keyof T)[]).reduce<
+    ErrorMessage<T>
+  >(
+    (acc, key) => {
+      acc[key] = "";
+      return acc;
+    },
+    { ...initialState }
+  );
+
+  const [errorMessage, setErrorMessage] =
+    useState<ErrorMessage<T>>(initialErrorMessage);
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setFormState({
+      ...formState,
+      [event.currentTarget.name]: getValue(event, formState),
+    });
+  };
+
+  const vaidateForm = () => {
+    const errorMessage = validateFn(formState);
+    setErrorMessage(errorMessage);
+    return Object.values(errorMessage).every((v) => v === "");
+  };
+
+  const handleSubmit = (onSubmit: (formData: T) => void) => {
+    return (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      setWasSubmitted(true);
+
+      onSubmit(formState);
+    };
+  };
+
+  const updateFormState = (formData: Partial<T>) => {
+    setFormState({
+      ...formState,
+      ...formData,
+    });
+  };
+
+  return {
+    formState,
+    errorMessage,
+    wasSubmitted,
+    handleChange,
+    vaidateForm,
+    handleSubmit,
+    setErrorMessage,
+    updateFormState,
+  };
+}
+
+function getValue(
+  event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  formState: Record<string, unknown>
+) {
+  switch (event.currentTarget.type) {
+    case "checkbox":
+      return typeof formState[event.currentTarget.name] === "boolean"
+        ? event.currentTarget.checked
+        : event.currentTarget.checked
+          ? event.currentTarget.value
+          : "";
+    case "number":
+      return Number(event.currentTarget.value);
+    default:
+      return event.currentTarget.value;
+  }
+}
--- exercise-notebook/ui/form/form-hook-controlled-form.tsx
+++ exercise-notebook/ui/form/form-hook-controlled-form.tsx
@@ -1,9 +1,11 @@
 "use client";
-import React, { ReactText, useState } from "react";
+import React from "react";
 import { useForm } from "./use-form";
 import { DateInput } from "@/form/components/date-input";
-import { SearchInput, SearchInputProps } from "@/form/components/search-input";
+import { SearchInput } from "@/form/components/search-input";
 import { Input } from "@/form/components/input";
+import { RadioInput } from "@/form/components/radio-input";
+import { Select } from "@/form/components/select";
 
 interface LoginFormData {
   id: string;
@@ -184,100 +186,6 @@ export default function FormHookControlledForm() {
       </Select>
       <button type="submit">로그인</button>
     </form>
-  );
-}
-
-interface SelectProps {
-  value: string;
-  errorMessage: string;
-  wasSubmitted: boolean;
-  label: string;
-  name: string;
-  onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
-  onBlur?: (event: React.FocusEvent<HTMLSelectElement>) => void;
-  onFocus?: (event: React.FocusEvent<HTMLSelectElement>) => void;
-}
-
-function Select({
-  name,
-  label,
-  value,
-  errorMessage,
-  wasSubmitted,
-  onChange,
-  children,
-  ...props
-}: React.PropsWithChildren<SelectProps>) {
-  return (
-    <div>
-      <label id={`label-${name}`} htmlFor={`input-${name}`}>
-        {label}
-      </label>
-      <select
-        {...props}
-        value={value}
-        name={name}
-        id={`input-${name}`}
-        onChange={onChange}
-        aria-labelledby={`label-${name}`}
-        aria-describedby={`error-${name}`}
-        aria-invalid={errorMessage ? true : false}
-      >
-        {children}
-      </select>
-      {errorMessage && wasSubmitted && (
-        <div id={`error-${name}`}>{errorMessage}</div>
-      )}
-    </div>
-  );
-}
-
-interface RadioInputProps {
-  value: string;
-  errorMessage: string;
-  wasSubmitted: boolean;
-  label: string;
-  name: string;
-  inputs: string[];
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-function RadioInput({
-  value,
-  name,
-  label,
-  wasSubmitted,
-  errorMessage,
-  inputs,
-  onChange,
-}: RadioInputProps) {
-  return (
-    <div>
-      <span>{label}</span>
-      {inputs.map((input, idx) => (
-        <label
-          key={input}
-          id={`label-${name}-${idx}`}
-          htmlFor={`input-${name}-${idx}`}
-        >
-          <input
-            type={"radio"}
-            name={name}
-            checked={value === input}
-            id={`input-${name}-${idx}`}
-            value={input}
-            onChange={onChange}
-            aria-labelledby={`label-${name}=${idx}`}
-            aria-describedby={`error-${name}`}
-            aria-invalid={errorMessage ? true : false}
-          />
-          {input}
-        </label>
-      ))}
-      {errorMessage && wasSubmitted && (
-        <div id={`error-${name}`}>{errorMessage}</div>
-      )}
-    </div>
   );
 }
 
