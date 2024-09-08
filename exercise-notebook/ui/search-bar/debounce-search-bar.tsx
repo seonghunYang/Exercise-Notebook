ada5c244bf6fa0cddbdfaddb7d04b36ad7a0f2d2
SeonghunYang <didtjdgns852@naver.com>
2024-09-08 07:54:34 +0000

    debounc로 검색 지연

---
+++ exercise-notebook/ui/search-bar/debounce-search-bar.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { stock_list } from "@/app/api/stocks/data";
+import { useFetch } from "./use-fetch";
+import { SearchInput } from "./search-input";
+
+type Stock = (typeof stock_list)[number];
+
+async function fetchStockList(url: string) {
+  const response = await fetch(url);
+  const { data } = await response.json();
+
+  return data;
+}
+
+export default function DebouncedSearchbar() {
+  const [query, setQuery] = useState("");
+  const debouncedQuery = useDebouceValue(query);
+  const { data: searchedStockList } = useFetch<Stock[]>({
+    url: `/api/stocks?query=${debouncedQuery}`,
+    fetcher: fetchStockList,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.currentTarget.value);
+  };
+
+  return (
+    <SearchInput value={query} onChange={handleChange}>
+      <div>검색 결과</div>
+      {searchedStockList?.map((stock) => (
+        <div key={stock.name}>{stock.name}</div>
+      ))}{" "}
+    </SearchInput>
+  );
+}
+
+function useDebouceValue(value: unknown, delay = 200) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 기존 타이머 제거
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
